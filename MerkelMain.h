03ad35d3f67ad34527053b3021367188eb0498a7
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-03 12:29:00 +0500

    Merkel Main header completed

--- MerkelMain.h
+++ MerkelMain.h
@@ -8,3 +8,18 @@ class MerkelMain{
 	public:
 		MerkelMain();
 		void init ();
+		
+	private:
+		void printMenu();
+		void loadOrderBook();
+		int getUserOption();
+		void printHelp();
+		void printExchangeStats();
+		void placeAsk();
+		void placeBid();
+		void printWallet();
+		void gotoNextTimeFrame();
+		void processUserOption(int userOption);
+		
+		std::vector<OrderBookEntry> entries;
+};

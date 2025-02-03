e595f30103ede765a38878132448735f4d0082fc
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-03 12:30:46 +0500

    Implementing init and constructor in merkel main cpp file

--- merkelmain.cpp
+++ merkelmain.cpp
@@ -16,3 +16,21 @@ void MerkelMain::init(){
 	}
 	
 }
+
+void MerkelMain::loadOrderBook(){
+	
+	entries.push_back(OrderBookEntry{
+        "10-1-2025/10:40:23.9908",	
+        "BTC/USDT",
+        OrderBookType::ask,
+        1000.01,
+        2000.901
+    });
+    entries.push_back(OrderBookEntry {
+        "15-2-2025/10:40:23",
+        "DOGE/USDT",
+        OrderBookType::bid,
+        100.051,
+        2000.991
+	)};
+}

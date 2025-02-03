f6e5c8fd42f8735fc09b17d28716ec7c7007a731
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-03 12:31:33 +0500

    Modifying functions

--- merkelmain.cpp
+++ merkelmain.cpp
@@ -34,3 +34,19 @@ void MerkelMain::loadOrderBook(){
         2000.991
 	)};
 }
+
+void MerkelMain::printMenu(){
+    std::cout << "1: Print  Help" << std::endl;
+    std::cout << "2: Print  exchange stat" << std::endl;
+    std::cout << "3: Place an ask" << std::endl;
+    std::cout << "4: Place a bid" << std::endl;
+    std::cout << "5: Print Wallet" << std::endl;
+    std::cout << "6: Continue" << std::endl;
+}
+
+int MerkelMain::getUserOption(){
+    int userOption;
+    std::cout << "Type in 1-6" << std::endl;
+    std::cin >> userOption;
+    return userOption;
+}

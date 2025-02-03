fdc7f826a515d8fb1ad9cce470dd26cde0cd42ad
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-03 12:29:49 +0500

    Making functions in merkel main cpp file

--- merkelmain.cpp
+++ merkelmain.cpp
@@ -1,1 +1,18 @@
-2
+#include "MerkelMain.h"
+
+#include <iostream>
+#include <string>
+
+void MerkelMain::init(){
+	
+	/** This is the main fuction of init*/
+	loadOrderBook();
+	int input;
+	
+	while (true){
+		printMenu();
+		input = getUserOption();
+		processUserOption(input);
+	}
+	
+}

11d97ba11b3406da539ea90a1873defaddfe28fa
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-04 21:13:29 +0500

    OrderEntry cpp file started

--- OrderBookEntry.cpp
+++ OrderBookEntry.cpp
@@ -5,3 +5,16 @@
 #include <string>
 
 OrderBookEntry::OrderBookEntry(
+        std::string _timeStamp,
+        std::string _product,
+        OrderBookType _orderType,
+        double _price,
+        double _amount) : 
+                        timeStamp(_timeStamp),
+                        product(_product),
+                        orderType(_orderType),
+                        price(_price),
+                        amount(_amount)
+    {
+    	
+    }

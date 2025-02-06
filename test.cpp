65af269ef1c9b3d8db2aac19369d844534471747
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-06 18:28:25 +0500

    testing some function in test file

--- test.cpp
+++ test.cpp
@@ -4,13 +4,36 @@
 
 std::vector<std::string> tokenise(std::string csvLine, char seperator){
 
+    std::vector<std::string> tokens;
+    signed int start,end;
+    std::string token;
 
+    start = csvLine.find_first_not_of(seperator,0);
 
+    do{
+        end= csvLine.find_first_of(seperator, start);
+        if (start == csvLine.length() || start == end ) break;
+        if(end>=0) token= csvLine.substr(start, end-start);
+        else token= csvLine.substr(start, csvLine.length()-start);
+        tokens.push_back(token);
+        start = end + 1 ;
+    }while (end > 0);
+
+    
+    return tokens;
 } 
 
 
 int main(){
 
+    std::vector<std::string> tokens;
+    std::string s = "thing,thing1,thing2,thing3";
+
+    tokens = tokenise(s, ',');
+
+    for(std::string& t : tokens){
+        std::cout<<t<<std::endl;
+    }
 
 
     return 0;

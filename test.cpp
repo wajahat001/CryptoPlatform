eefade8ae02fb0ddb58e539e96c8e233d8f0c063
wajahat001 <wajahatchaudy1640@gmail.com>
2025-02-09 21:03:26 +0500

    fixing bugs

--- test.cpp
+++ test.cpp
@@ -48,10 +48,22 @@ int main(){
         {
             std::cout<< line << std::endl;
             tokens = tokenise(line , ',');
-            double price = std::stod(tokens[3]);
-            double amount = std::stod(tokens[4]);
-            std::cout<< price << " : " << amount << std::endl;
-
+            if(tokens.size() != 5){
+                std::cout<<"Bad Line! Not Enough Values"<<std::endl;
+                continue;
+            }
+            try
+            {
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+                std::cout<< price << " : " << amount << std::endl;
+            }
+            catch(std::exception& e)
+            {
+                std::cout<<"There is Invalid double!"<< tokens[3] <<std::endl;
+                std::cout<<"There is Invalid double!"<< tokens[4] <<std::endl;
+                continue;
+            }
         }
         DataFile.close();
     }
